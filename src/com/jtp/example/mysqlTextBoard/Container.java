06eef01ccd9056903ae3e815e4bc64ab4d287df8
jtpark1957 <jtpark1957@gmail.com>
2021-01-13 17:02:00 +0900

    Service를 컨테이너에서 공급받기

--- src/com/jtp/example/mysqlTextBoard/Container.java
+++ src/com/jtp/example/mysqlTextBoard/Container.java
@@ -2,11 +2,18 @@ package com.jtp.example.mysqlTextBoard;
 
 import java.util.Scanner;
 
+import com.jtp.example.mysqlTextBoard.service.ArticleService;
+import com.jtp.example.mysqlTextBoard.service.MemberService;
+
 public class Container {
 
 	public static Scanner scanner;
+	public static MemberService memberService;
+	public static ArticleService articleService;
 	
 	static {
 		scanner = new Scanner(System.in);
+		memberService = new MemberService();
+		articleService = new ArticleService();
 	}
 }
